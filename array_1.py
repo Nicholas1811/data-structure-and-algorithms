36d96f306aac9476b50333de222a2b107d943889
Nicholas Soh <nicholassdl660@gmail.com>
2024-12-02 13:51:54 +0800

    arr some stuff

--- array_1.py
+++ array_1.py
@@ -1,2 +1,13 @@
+#1D array
 arr = [1,2,3,4,5,6,7,8,9,10]
 
+#Accessing array: Usage of index O(1)
+def pos_num(ind):
+    return arr[ind]
+
+def add_array():
+    arr.insert(11,len(arr)+1)
+    return arr
+
+print(pos_num(1))
+print(add_array())
