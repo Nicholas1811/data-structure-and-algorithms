e5a7041b2315763d99d159b975ece5bb779dc582
Nicholas Soh <nicholassdl660@gmail.com>
2024-12-06 14:07:58 +0800

    done adding from front, back and printing values in linked list
     Have to continue of adding by key and understand how add_new back works

--- linked_list.py
+++ linked_list.py
@@ -5,34 +5,68 @@ class Node:
         self.data = data
         self.next = None
         
-#Creation of the actual link list. Example from https://www.w3schools.com/dsa/dsa_data_linkedlists_types.php
-node_1 = Node(10)
-node_2 = Node(20)
-node_3 = Node(30)
-node_4 = Node(40)
+#Creating of linked list with a LinkedList Class
+class LinkedList:
+    #When linked list is initialised, the head is none.
+    #Adding can be done in 3 ways. Add at the front, middle, or back
 
-node_1.next = node_2
-node_2.next = node_3
-node_3.next = node_4
+    def __init__ (self):
+        self.head = None
+    
+    #Adding at the front involves 3 steps
+    #Firstly, get the new data and set it to the node class so it should have (Data, Node)
+    #Secondly, check if head exists. If head does not exists, set the head as the new_data
+    #If the head exists, set new_data reference to the head and then, set the new_data as the head.
+    def add_new_front(self,data):
+        new_node = Node(data) # From value 20 becomes (20,next value)
+        if self.head == None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head  #Setting the next value to the second value
+            self.head = new_node
 
-currentNode = node_1
-while currentNode:
-    print(currentNode.data, end=" -> ")
-    currentNode = currentNode.next
 
+    #Adding at the back is slightly more complicated
+    #Firstly, can copy the steps, if head is none, it can just set.
+    #If the head is occupied, we will have to tranverse the LinkedList.
 
+    def add_new_back(self,data):
+        new_node = Node(data)
+        if self.head == None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+    
+    #Adding by specific key is similar as well.
+    #Takes in the value, and we can add a counter
 
-#Class to create the actual linked list
-# class LinkedList:
-#     def insert(self,data):
-#         #Creates a new node for the data.
-#         new_data = Node(data)
-#         #If there is nothing in the head of the LinkedList, it will set the data node as the head.
-#         if self.head is None:
-#             self.head = new_data
-#             return
-#         else:
-#         #If there is something in the head of the LinkedList, meaning a node,  
-#             new_data.next = self.head
-#             self.head = new_data
+    def add_by_key(self,data,key):
+        new_node = Node(data)
+        if self.head == None:
+            self.head = new_node
+        else:
+            current = self.head
+            count = 1 #Tracking of index
+            while current:
+                current = current.next
+                count+=1
+                if count == key:
 
+    def print_linked_list(self):
+        current = self.head #Setting of self.head to current variable. self.head should be the first element of the LinkedList.
+        while current: #While the variable current is not empty, it will print the current.data first and then, sets the current value to current.next
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+        
+
+new_ll = LinkedList()
+new_ll.add_new_front(10)
+new_ll.add_new_front(20)
+new_ll.add_new_front(30)
+new_ll.add_new_back(100)
+new_ll.print_linked_list()
